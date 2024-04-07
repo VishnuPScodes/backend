e231d697225f967847f38567c6da69d3e4b90ba7
VishnuPS <psvishnu373@gmail.com>
2024-04-07 14:52:20 +0530

    started working with mySQl Project

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,13 +1,5 @@
 import mongoose, { Types } from 'mongoose';
 
-export interface IUser extends Document {
-  userName: string;
-  email: string;
-  profilePicture: string;
-  followers: Types.ObjectId[];
-  following: Types.ObjectId[];
-}
-
 export const userSchema = new mongoose.Schema({
   userName: {
     type: String,
@@ -28,4 +20,4 @@ export const userSchema = new mongoose.Schema({
   },
 });
 
-export const User = mongoose.model<IUser>('user', userSchema);
+export const User = mongoose.model('user', userSchema);
